f7727676519eb4523da4ecd3ad20c2742ca34fc0
Uriel Kumordzie <kumordzieesinu@gmail.com>
2023-02-21 14:54:50 -0800

    02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,7 @@
+#include "urieltsucks.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
