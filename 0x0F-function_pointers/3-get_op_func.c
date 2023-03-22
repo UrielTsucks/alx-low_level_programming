ce4275109100e595ccc9504c352ee652ee189fb7
Uriel Kumordzie <kumordzieesinu@gmail.com>
2023-03-22 14:03:29 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,48 +1,33 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
-
+#include"3-calc.h"
 /**
-<<<<<<< HEAD
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
- */
-=======
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
+(* a blank line
+* Description: get the operator and redirect to the func.)?
+(* section header: 3-calc.h)*
+* Return: return null or the function to do.
+*/
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-<<<<<<< HEAD
-	{ "/", op_div }
-=======
-	{ "/", op_div },
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+		{
+			return (*(ops[i]).f);
+		}
 		i++;
 	}
-
-	return (0);
+	return (NULL);
 }
